cad448fe40928913a2d6b92004c573fb84aec1b2
Monu <kdie44@gmail.com>
2021-09-25 14:09:54 +0900

    firebase to fbase, js 파일 코드 수정

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ Attempted import error: './firebase' does not contain a default export (imported
 ```
 export가 설정되지 않았다는 설명에 firebase.js 파일 하단에 `export default app;` 추가로 해결
 
+## 09.23
+로그인 기능 추가
+절대 경로의 아쉬운 점, `npm install`로 설치한 패키지 이름이 파일 이름과 같으면 오류가 발생한다. 
+* `firebase.js->fbase.js`
+`jsconfig.json` src 폴더를 기준으로 폴더나 파일 경로를 적을 수 있어 import 문의 가독성이 좋아진다
+
+## 09.25
+AppRouter 컴포넌트가 라우터 역할을 하면서도 코드의 가독성까지 챙기기 위해서는 App컴포넌트에서 관리하는 것이 좋다.
+JSX에 자바스크립트 코드를 삽입할 때는 코드를 중괄호로 감싸줘야한다.
+
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,3 +1,25 @@
-import { HashRouter as Router,
+import { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
-}
+const AppRouter = ({ isLoggedIn }) => {//상위 컴포넌트에서 받은 프롭스는 구조 분해 할당으로 사용
+    //코드 실행시 isLoggedIn은 setIsLoggedIn으로 관리하는 상태로 취급, 초깃값은 false, 로그인 값을 변경할 때 사용
+    return (
+        <Router>
+            <Switch>
+                {isLoggedIn ? (
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                ) : (
+                    <Route exact path="/">
+                        <Auth />
+                    </Route>
+                )}
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
