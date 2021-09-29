496b6d08ded00863994bc37fc12215b7d398d724
Monu <kdie44@gmail.com>
2021-09-30 03:00:03 +0900

    db연결

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,16 +3,21 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState("");
 
     const getNweets = async () => {
         const dbNweets = await dbService.collection("nweets").get();
-        console.log(dbNweets);
+        dbNweets.forEach((document) => {
+            const nweetObject = { ...document.data(), id: document.id };
+            setNweets((prev) => [nweetObject, ...prev])
+        });
     };
 
     useEffect(() => {
         getNweets();
     }, []);
 
+    console.log(nweets);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
